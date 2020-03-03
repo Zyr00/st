25c047565a7beced5e1fb28e7f289a29932d0159
João Cunha <skidrow.jc@gmail.com>
2020-03-03 12:55:03 +0000

    Clipboard suport

--- x.c
+++ x.c
@@ -660,6 +660,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+        clipcopy(NULL);
 }
 
 void
@@ -678,7 +680,9 @@ brelease(XEvent *e)
 
 	if (mouseaction(e, 1))
 		return;
-	if (e->xbutton.button == Button1)
+        if (e->xbutton.button == Button3)
+                clippaste(NULL);
+        else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
 
