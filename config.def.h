448d9339635747b55cdd66b5a6a76f3a6172afdc
João Cunha <skidrow.jc@gmail.com>
2020-06-12 20:31:16 +0100

    Config Update

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Fira Code:pixelsize=10:antialias=true:autohint=true";
-static int borderpx = 4;
+static int borderpx = 2;
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
